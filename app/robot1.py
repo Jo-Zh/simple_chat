36584d31a4d66fb8a1b97407ba141b7eae8e21b5
Jo-Zh <joannaweinazh@gmail.com>
2023-05-10 20:41:32 +1000

    init

---
+++ app/robot1.py
@@ -0,0 +1,48 @@
+import os
+import openai 
+import streamlit as st
+
+# pip install streamlit-chat  
+from streamlit_chat import message
+
+openai.api_key = os.getenv("CHATAPI_KEY")
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+#Creating the chatbot interface
+st.title("chatBot : Streamlit + openAI")
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+#  get the user's input and click send button
+user_input = st.text_input("You","",key="input") #get_text()
+submit_button=st.button("send")
+
+if submit_button and user_input:
+    output = generate_response(user_input)
+    # store the output 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+    user_input=""
+
+
+if st.session_state['generated']:
+    
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
